23e39d51930bce9fb1a06011a2df5923c8769d85
Marko Zivanovic <zivanovic.markko002@gmail.com>
2024-06-18 13:08:39 +0200

    Added few new albums

--- src/main/java/com/example/vinyl/vibes/migration/V1__init__data.java
+++ src/main/java/com/example/vinyl/vibes/migration/V1__init__data.java
@@ -48,6 +48,12 @@ public class V1__init__data {
                     .name("Kanye West")
                     .build()));
         }
+        Optional<Artist> artist4 = artistRepository.findByName("Travis Scott");
+        if (artist4.isEmpty()) {
+            artist4 = Optional.of(artistRepository.save(Artist.builder()
+                    .name("Travis Scott")
+                    .build()));
+        }
 
 
         if (albumRepository.findAll().isEmpty()) {
@@ -60,12 +66,22 @@ public class V1__init__data {
                     image("https://images.prismic.io/milanote/df7eeb83a07162b45ac2e882cac055de9411054a_cover.jpg?auto=compress,format")
                     .year(1991).artist(artist2.get()).genre(Genre.ROCK).deleted(false).
                     description("It includes anti-establishment views, anti-sexism, frustration, alienation, and troubled love inspired by Cobain's " +
-                            "broken relationship with Bikini Kill's Tobi Vail.").dislikes(55).likes(221).build());
+                            "broken relationship with Bikini Kill's Tobi Vail.").dislikes(3).likes(15).build());
             albumRepository.save(Album.builder().name("Graduation").
                     image("https://m.media-amazon.com/images/I/71T4fEX7jaL._AC_UF1000,1000_QL80_.jpg")
                     .year(2007).artist(artist3.get()).genre(Genre.HIP_HOP).deleted(false).
                     description("GRADUATION is the 3rd installment in the Kanye West series of ground breaking albums, targeting every school kid," +
-                            " from those that have dropped out of collage.").dislikes(21).likes(341).build());
+                            " from those that have dropped out of collage.").dislikes(2).likes(5).build());
+            albumRepository.save(Album.builder().name("Utopia").
+                    image("https://m.media-amazon.com/images/I/51nMgSxPQeL._UF1000,1000_QL80_.jpg")
+                    .year(2023).artist(artist4.get()).genre(Genre.HIP_HOP).deleted(false).
+                    description("Utopia is the fourth studio album by American rapper Travis Scott. It was released through Cactus Jack and Epic Records" +
+                            " on July 28, 2023. Utopia divided reviewers.").dislikes(1).likes(12).build());
+            albumRepository.save(Album.builder().name("Kids see ghosts").
+                    image("https://cdn-0.atwoodmagazine.com/wp-content/uploads/2018/06/uaNIo6X-1050x700.jpg")
+                    .year(2018).artist(artist3.get()).genre(Genre.HIP_HOP).deleted(false).
+                    description("Kids See Ghosts is the debut studio album by the American hip hop supergroup Kids See Ghosts, composed of the rappers and " +
+                            "producers Kanye West and Kid Cudi. ").dislikes(0).likes(2).build());
         }
     }
 }
